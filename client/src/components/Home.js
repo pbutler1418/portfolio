11358151e014ced109bac1f2c165462bcc5d4866
Pierce Butler <piercebutler@Pierces-MacBook-Air.local>
2020-08-10 11:41:45 -0400

    updated portfolio site

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,19 +1,21 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
-
 function Home() {
-  
   return (
     <>
       <div className="container"></div>
       <div className="homepage">
         <div className="intro">
+          <div className="whiteStripe"></div>
           <h1 className="firstPageDivs">Pierce Butler</h1>
+
+          <div className="whiteStripe"></div>
           <h1 className="firstPageDivs">Software Engineer</h1>
+
+          <div className="whiteStripe"></div>
           <h1 className="firstPageDivs">Front-end Enthusiast</h1>
         </div>
-        
       </div>
     </>
   );
